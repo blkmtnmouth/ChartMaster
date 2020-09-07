3aae6b9dc606bff3e415f58af987975df422ec56
Bryan Cook <brykcook@gmail.com>
2020-09-07 15:46:43 -0400

    Heroku setup

--- app/config/connection.js
+++ app/config/connection.js
@@ -12,5 +12,16 @@ var sequelize = new Sequelize("graph_master_db", "root", "", {
   }
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
+
 // Exports the connection for other files to use
 module.exports = sequelize;
